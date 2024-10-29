0ad7e7a599ca7da7f9f2f06b625dc586ac54f212
Thomas Camminady <thomas@camminady.dev>
2024-10-29 17:12:38 +0100

    Initial commit.

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for coding_purgatory."""
---
+++ tests/test_true.py
@@ -0,0 +1,6 @@
+"""Tests for `coding_purgatory` package."""
+
+
+def test_true() -> None:
+    """At least one test will always pass :)."""
+    assert True
